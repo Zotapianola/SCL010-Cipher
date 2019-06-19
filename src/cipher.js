d03c8af3b3012f09797a9870a9d3088d80e76061
Estefanía Romero-Cors <zotapianola@gmail.com>
2019-06-19 10:53:53 -0400

    Primer theme listo. Interacciones con usuario listas.

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,39 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
+
+  encode: (offset, secretMsg) => {
+
+    // desmembra string y lo transforma en Array
+    secretMsg = secretMsg.split('');
+    let newPosition;
+
+    // por cada vuelta, muestra última letra ya desplazada
+    for (let i = 0; i < secretMsg.length; i++)
+    {
+      // en este momento no loopea
+      newPosition = (secretMsg[i].charCodeAt() - 65 + offset) % 26 + 65;
+      secretMsg[i] = String.fromCharCode(newPosition);
+    }
+    // juntar de nuevo
+    secretMsg = secretMsg.join('');
+    return secretMsg;
+
+
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (offset,secretMsg) => {
+    let newPosition;
+
+    // desmembra string y lo transforma en Array
+    secretMsg = secretMsg.split('');
+
+    // por cada vuelta, muestra última letra ya desplazada
+    for (let i = 0; i < secretMsg.length; i++)
+    {
+      // en este momento no loopea
+      newPosition = (secretMsg[i].charCodeAt() - 90 - offset) % 26 + 90;
+      secretMsg[i] = String.fromCharCode(newPosition);
+    }
+    // juntar de nuevo
+    secretMsg = secretMsg.join('');
+    return secretMsg;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
-/*Acá va tu código*/
+// guardar user input
+let secretMsg = "";
+// valor temporal de offset
+let offset;
+
+// textarea de DOM
+const textArea = document.getElementById("textarea");
+
+// bool que chequea si usa encode o decode
+let isEncoding = true;
+
+// lógica encode/decode
+const encDecBtn = document.getElementById("encDecB");
+const magicEye = document.getElementById("h1Eye");
+encDecBtn.addEventListener("click", function encodeDecode()
+{
+  if (isEncoding)
+  {
+    encode();
+    encDecBtn.innerText = "Decodificar";
+    magicEye.innerText = "㊀";
+    isEncoding = false;
+  }
+  else {
+    decode();
+    magicEye.innerText = "◉";
+    encDecBtn.innerText = "Codificar";
+    isEncoding = true;
+  }
+});
+
+// lógica botón select/copy text
+const copyBtn = document.getElementById("copyToClpB");
+copyBtn.addEventListener("click", function copyToClipboard(){
+  let copyText = textArea;
+  copyText.select();
+  document.execCommand("copy");
+});
+
+function encode ()
+{
+  // obtiene valores del usuario
+  offset = parseInt(document.getElementById("userOffset").value);
+  secretMsg = textArea.value.toUpperCase();
+
+  // muestra resultado decodificado
+  textArea.value = cipher.encode(offset,secretMsg);
+}
+
+
+function decode ()
+{
+  // obtiene valores del usuario
+  offset = parseInt(document.getElementById("userOffset").value);
+  secretMsg = textArea.value.toUpperCase();
+
+  // muestra resultado decodificado
+  textArea.value = cipher.decode(offset,secretMsg);
+}

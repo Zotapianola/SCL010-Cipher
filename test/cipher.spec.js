25c6af75ba16a917bb55b7b83dd0a9e8bdecbab7
Estefanía Romero-Cors <zotapianola@gmail.com>
2019-06-23 00:41:03 -0400

    últimas pruebas unitarias

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -17,7 +17,21 @@ describe('cipher', () => {
     it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcefghijklmnopqrstuvwxyz" con offset 33', () => {
       assert.equal(cipher.encode(33, "abcdefghijklmnopqrstuvwxyz"), "hijklmnopqrstuvwxyzabcdefg");
     });
-
+    it('debería retornar "BIBCO" para "ÑUÑOA" con offset 66', () => {
+      assert.equal(cipher.encode(66, "ÑUÑOA"), "BIBCO");
+    });
+    it('debería retornar "bibco" para "ñuñoa" con offset 66', () => {
+      assert.equal(cipher.encode(66, "ñuñoa"), "bibco");
+    });
+    it('debería retornar "23456789:12" para "12345678901" con offset de 33', () => {
+      assert.equal(cipher.encode(33, "12345678901"), "23456789:12");
+    });
+    it('debería retornar "" para "  " con offset de 9 ', () => {
+      assert.equal(cipher.encode(9,"  "), "");
+    });
+    it('debería retornar "RozbdFgnrsBnzrssnBnzrs4" para "Space Ghost Coast to Coast!" con offset de 467', () => {
+      assert.equal(cipher.encode(467, "Space Ghost Coast to Coast!"), "RozbdFgnrsBnzrssnBnzrs4");
+    });
   });
 
   describe('cipher.decode', () => {
@@ -25,13 +39,20 @@ describe('cipher', () => {
     it('debería ser una función', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
-
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
       assert.equal(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     });
-
     it('debería retornar "abcefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
       assert.equal(cipher.decode(33, "hijklmnopqrstuvwxyzabcdefg"), "abcdefghijklmnopqrstuvwxyz");
+    });
+    it('debería retornar "12345678901" para "23456789:12" con offset de 33', () => {
+      assert.equal(cipher.decode(33, "23456789:12"), "12345678901");
+    });
+    it('debería retornar "  " para "" con offset de 9 ', () => {
+      assert.equal(cipher.decode(9, ""), "  ");
+    });
+    it('debería retornar "Space Ghost Coast to Coast!" para "RozbdFgnrsBnzrssnBnzrs4" con offset de 467', () => {
+      assert.equal(cipher.decode(467, "RozbdFgnrsBnzrssnBnzrs4"), "Space Ghost Coast to Coast!");
     });
 
   });
